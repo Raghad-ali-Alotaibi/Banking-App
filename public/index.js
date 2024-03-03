5a0ff502e96f4fa7b3f704d4caa4d0826c5cd328
Raghad-ali-Alotaibi <raghad.ali.alotaibi@gmail.com>
2024-03-03 13:47:40 +0300

    add some format to method

--- public/index.js
+++ public/index.js
@@ -1,131 +1,133 @@
 "use strict";
 //Transaction
 class Transaction {
-    constructor(amount) {
-        this.amount = amount;
-        this.date = new Date();
-    }
+  constructor(amount) {
+    this.amount = amount;
+    this.date = new Date();
+  }
 }
 // Customer
 class Customer {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
-        this.transactions = [];
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+    this.transactions = [];
+  }
+  getName() {
+    return this.name;
+  }
+  getId() {
+    return this.id;
+  }
+  getTransactions() {
+    return this.transactions;
+  }
+  getBalance() {
+    return this.transactions.reduce(
+      (balance, transaction) => balance + transaction.amount,
+      0
+    );
+  }
+  addTransactions(amount) {
+    const transaction = new Transaction(amount);
+    if (this.validateTransaction(amount)) {
+      this.transactions.push(transaction);
+      return true;
     }
-    getName() {
-        return this.name;
+    return false;
+  }
+  // Function to validate Transaction
+  validateTransaction(amount) {
+    if (typeof amount !== "number" || amount < 0) {
+      console.log("Invalid transaction amount");
+      return false;
     }
-    getId() {
-        return this.id;
-    }
-    getTransactions() {
-        return this.transactions;
-    }
-    getBalance() {
-        return this.transactions.reduce((balance, transaction) => balance + transaction.amount, 0);
-    }
-    addTransactions(amount) {
-        const transaction = new Transaction(amount);
-        if (this.validateTransaction(amount)) {
-            this.transactions.push(transaction);
-            return true;
-        }
-        return false;
-    }
-    // Function to validate Transaction
-    validateTransaction(amount) {
-        if (typeof amount !== "number" || amount < 0) {
-            console.log("Invalid transaction amount");
-            return false;
-        }
-        return true;
-    }
+    return true;
+  }
 }
 //Branch
 class Branch {
-    constructor(name) {
-        this.name = name;
-        this.customers = [];
+  constructor(name) {
+    this.name = name;
+    this.customers = [];
+  }
+  getName() {
+    return this.name;
+  }
+  getCustomers() {
+    return this.customers;
+  }
+  addCustomer(customer) {
+    if (!this.customers.includes(customer)) {
+      const result = this.customers.push(customer);
+      return result > 0 ? true : false;
     }
-    getName() {
-        return this.name;
+    return false;
+  }
+  addCustomerTransaction(customerId, amount) {
+    const customer = this.customers.find(
+      (customer) => customer.id === customerId
+    );
+    if (customer) {
+      customer.addTransactions(amount);
+      return true;
+    } else {
+      return false;
     }
-    getCustomers() {
-        return this.customers;
-    }
-    addCustomer(customer) {
-        if (!this.customers.includes(customer)) {
-            const result = this.customers.push(customer);
-            return result > 0 ? true : false;
-        } else
-        return false;
-}
-    addCustomerTransaction(customerId, amount) {
-        const customer = this.customers.find((customer) => customer.id === customerId);
-        if (customer) {
-            customer.addTransactions(amount);
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    // Function to search for customers by name
-    searchCustomers(keyword) {
-        const results = this.customers.filter((customer) => {
-            return customer.name.toLowerCase().includes(keyword.toLowerCase());
-        });
-        return results;
-    }
+  }
+  // Function to search for customers by name
+  searchCustomers(keyword) {
+    const results = this.customers.filter((customer) => {
+      return customer.name.toLowerCase().includes(keyword.toLowerCase());
+    });
+    return results;
+  }
 }
 // bank
 class Bank {
-    constructor(name) {
-        this.name = name;
-        this.branches = [];
+  constructor(name) {
+    this.name = name;
+    this.branches = [];
+  }
+  addBranch(branch) {
+    if (!this.branches.includes(branch)) {
+      const result = this.branches.push(branch);
+      return result > 0 ? true : false;
     }
-    addBranch(branch) {
-        if (!this.branches.includes(branch)) {
-            const result = this.branches.push(branch);
-            return result > 0 ? true : false;
-        }
-        else
-            return false;
+    return false;
+  }
+  addCustomer(branch, customer) {
+    if (this.branches.includes(branch)) {
+      return branch.addCustomer(customer);
+    } else {
+      return false;
     }
-    addCustomer(branch, customer) {
-        if (this.branches.includes(branch)) {
-            return branch.addCustomer(customer);
-        }
-        else {
-            return false;
-        }
+  }
+  addCustomerTransaction(branch, customerId, amount) {
+    if (this.branches.includes(branch)) {
+      return branch.addCustomerTransaction(customerId, amount);
+    } else {
+      return false;
     }
-    addCustomerTransaction(branch, customerId, amount) {
-        if (this.branches.includes(branch)) {
-            return branch.addCustomerTransaction(customerId, amount);
-        }
-        else {
-            return false;
-        }
+  }
+  findBranchByName(branchName) {
+    return this.branches.find((branch) => branch.name === branchName);
+  }
+  checkBranch(branch) {
+    return this.branches.includes(branch);
+  }
+  listCustomers(branch, includeTransactions) {
+    if (this.branches.includes(branch)) {
+      if (includeTransactions) {
+        console.log(branch.customers);
+      } else {
+        branch.customers.map(
+          (customer, object) =>
+            (object = { id: customer.id, name: customer.name })
+        );
+      }
+    } else {
+      console.log("The Branch not found !!");
     }
-    findBranchByName(branchName) {
-        return this.branches.find((branch) => branch.name === branchName);
-    }
-    checkBranch(branch) {
-        return this.branches.includes(branch);
-    }
-    listCustomers(branch, includeTransactions) {
-        if (this.branches.includes(branch)) {
-            if (includeTransactions) {
-                console.log(branch.customers);
-            }
-            else {
-                branch.customers.map((customer, object) => (object = { id: customer.id, name: customer.name }));
-            }
-        }
-        else {
-            console.log("The Branch not found !!");
-        }
-    }
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,144 +1,141 @@
 //Transaction
 class Transaction {
-    amount: number;
-    date: Date;
-    constructor(amount: number) {
-      this.amount = amount;
-      this.date = new Date();
-    }
+  amount: number;
+  date: Date;
+  constructor(amount: number) {
+    this.amount = amount;
+    this.date = new Date();
   }
-  // Customer
-  class Customer {
-    name: string;
-    id: number;
-    transactions: Transaction[];
-    constructor(name: string, id: number) {
-      this.name = name;
-      this.id = id;
-      this.transactions = [];
-    }
-    getName() {
-      return this.name;
-    }
-    getId() {
-      return this.id;
-    }
-    getTransactions() {
-      return this.transactions;
-    }
-    getBalance() {
-      return this.transactions.reduce(
-        (balance, transaction) => balance + transaction.amount,
-        0
-      );
-    }
-    addTransactions(amount: number): boolean {
-      const transaction = new Transaction(amount);
-      if (this.validateTransaction(amount)) {
-        this.transactions.push(transaction);
-        return true;
-      }
-      return false;
-    }
-    // Function to validate Transaction
-    validateTransaction(amount: number): boolean {
-      if (typeof amount !== "number" || amount < 0) {
-        console.log("Invalid transaction amount");
-        return false;
-      }
+}
+// Customer
+class Customer {
+  name: string;
+  id: number;
+  transactions: Transaction[];
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+    this.transactions = [];
+  }
+  getName() {
+    return this.name;
+  }
+  getId() {
+    return this.id;
+  }
+  getTransactions() {
+    return this.transactions;
+  }
+  getBalance() {
+    return this.transactions.reduce(
+      (balance, transaction) => balance + transaction.amount,
+      0
+    );
+  }
+  addTransactions(amount: number): boolean {
+    const transaction = new Transaction(amount);
+    if (this.validateTransaction(amount)) {
+      this.transactions.push(transaction);
       return true;
     }
+    return false;
   }
-  
-  //Branch
-  class Branch {
-    name: string;
-    customers: Customer[];
-    constructor(name: string) {
-      this.name = name;
-      this.customers = [];
-    }
-    getName() {
-      return this.name;
-    }
-    getCustomers() {
-      return this.customers;
-    }
-    addCustomer(customer: Customer) {
-      if (!this.customers.includes(customer)) {
-        const result = this.customers.push(customer);
-        return result > 0 ? true : false;
-      }else
+  // Function to validate Transaction
+  validateTransaction(amount: number): boolean {
+    if (typeof amount !== "number" || amount < 0) {
+      console.log("Invalid transaction amount");
       return false;
+    }
+    return true;
+  }
 }
-    addCustomerTransaction(customerId: number, amount: number) {
-      const customer = this.customers.find(
-        (customer) => customer.id === customerId
-      );
-      if (customer) {
-        customer.addTransactions(amount);
-        return true;
-      } else {
-        return false;
-      }
+
+//Branch
+class Branch {
+  name: string;
+  customers: Customer[];
+  constructor(name: string) {
+    this.name = name;
+    this.customers = [];
+  }
+  getName() {
+    return this.name;
+  }
+  getCustomers() {
+    return this.customers;
+  }
+  addCustomer(customer: Customer) {
+    if (!this.customers.includes(customer)) {
+      const result = this.customers.push(customer);
+      return result > 0 ? true : false;
+    } else return false;
+  }
+  addCustomerTransaction(customerId: number, amount: number) {
+    const customer = this.customers.find(
+      (customer) => customer.id === customerId
+    );
+    if (customer) {
+      customer.addTransactions(amount);
+      return true;
     }
-    // Function to search for customers by name
-    searchCustomers(keyword: string) {
-      const results = this.customers.filter((customer) => {
-        return customer.name.toLowerCase().includes(keyword.toLowerCase());
-      });
-      return results;
+    return false;
+  }
+  // Function to search for customers by name
+  searchCustomers(keyword: string) {
+    const results = this.customers.filter((customer) => {
+      return customer.name.toLowerCase().includes(keyword.toLowerCase());
+    });
+    return results;
+  }
+}
+
+// bank
+class Bank {
+  name: string;
+  branches: Branch[];
+  constructor(name: string) {
+    this.name = name;
+    this.branches = [];
+  }
+  addBranch(branch: Branch): boolean {
+    if (!this.branches.includes(branch)) {
+      const result: number = this.branches.push(branch);
+      return result > 0 ? true : false;
+    }
+    return false;
+  }
+  addCustomer(branch: Branch, customer: Customer) {
+    if (this.branches.includes(branch)) {
+      return branch.addCustomer(customer);
+    } else {
+      return false;
     }
   }
-  
-  // bank
-  class Bank {
-    name: string;
-    branches: Branch[];
-    constructor(name: string) {
-      this.name = name;
-      this.branches = [];
-    }
-    addBranch(branch:Branch):boolean{
-      if(!this.branches.includes(branch)){
-          const result:number = this.branches.push(branch);
-          return result > 0 ? true : false;
-      }
-      else return false;
-  }
-    addCustomer(branch: Branch, customer: Customer) {
-      if (this.branches.includes(branch)) {
-        return branch.addCustomer(customer);
-      } else {
-        return false;
-      }
-    }
-    addCustomerTransaction(branch: Branch, customerId: number, amount: number) {
-      if (this.branches.includes(branch)) {
-        return branch.addCustomerTransaction(customerId, amount);
-      } else {
-        return false;
-      }
-    }
-    findBranchByName(branchName:string) {
-      return this.branches.find((branch) => branch.name === branchName);
-    }
-    checkBranch(branch:Branch) {
-      return this.branches.includes(branch);
-    }
-    listCustomers(branch: Branch, includeTransactions: boolean) {
-      if (this.branches.includes(branch)) {
-        if (includeTransactions) {
-          console.log(branch.customers);
-        } else {
-          branch.customers.map(
-            (customer, object: {}) =>
-              (object = { id: customer.id , name: customer.name })
-          );
-        }
-      } else {
-        console.log("The Branch not found !!");
-      }
+  addCustomerTransaction(branch: Branch, customerId: number, amount: number) {
+    if (this.branches.includes(branch)) {
+      return branch.addCustomerTransaction(customerId, amount);
+    } else {
+      return false;
     }
   }
-  
+  findBranchByName(branchName: string) {
+    return this.branches.find((branch) => branch.name === branchName);
+  }
+  checkBranch(branch: Branch) {
+    return this.branches.includes(branch);
+  }
+  listCustomers(branch: Branch, includeTransactions: boolean) {
+    if (this.branches.includes(branch)) {
+      if (includeTransactions) {
+        console.log(branch.customers);
+      } else {
+        branch.customers.map(
+          (customer, object: {}) =>
+            (object = { id: customer.id, name: customer.name })
+        );
+      }
+    } else {
+      console.log("The Branch not found !!");
+    }
+  }
+}
